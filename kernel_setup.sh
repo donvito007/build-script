cfbbee10138d88c726f6ac94cce15eb712f9e3d7
Diaz1401 <84665900+Diaz1401@users.noreply.github.com>
2021-06-13 22:42:14 +0700

    Update kernel_setup.sh

--- kernel_setup.sh
+++ kernel_setup.sh
@@ -13,12 +13,14 @@ KERNEL_DIR="$CURRENT_DIR"
 AK_REPO="https://github.com/Diaz1401/AnyKernel3"
 AK_DIR="$HOME/AnyKernel3"
 TC_DIR="$HOME"
+BRANCH_ARM64="63184f4c4963fe303abd9cae6c05df5222cb8229"
+BRANCH_ARM="c74ca949048d1421522afc24748dbda9b70ec924"
 # End Config
 
 # clone_tc - clones proton clang to TC_DIR
 clone_tc() {
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 --branch 63184f4c4963fe303abd9cae6c05df5222cb8229 --single-branch $TC_DIR/arm64
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm --branch c74ca949048d1421522afc24748dbda9b70ec924 --single-branch $TC_DIR/arm
+	wget https://github.com/mvaisakh/gcc-arm64/archive/$BRANCH_ARM64.zip && unzip $BRANCH_ARM64.zip && mv -f gcc-arm64-$BRANCH_ARM64 $TC_DIR/arm64
+	wget https://github.com/mvaisakh/gcc-arm/archive/$BRANCH_ARM.zip && unzip $BRANCH_ARM.zip && mv -f gcc-arm-$BRANCH_ARM.zip $TC_DIR/arm
         git clone --depth=1 https://github.com/kdrag0n/proton-clang $TC_DIR/clang
         cd $TC_DIR/clang
         sudo cp -rf bin /usr/
