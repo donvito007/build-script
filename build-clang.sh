a6a1900dca4fd69db009a1ad5189f46fd742c47f
Diaz1401 <reagor8161@outlook.com>
2022-04-22 22:32:40 +0700

    clang: Update coding style
    * Cosmetic only
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- build-clang.sh
+++ build-clang.sh
@@ -3,19 +3,19 @@
 # Copyright (c) 2021 CloudedQuartz
 # Copyright (c) 2021-2022 Diaz1401
 
-KERNEL_NAME="Kucing"
-KERNEL_DIR="$(pwd)"
-AK3="$KERNEL_DIR/AnyKernel3"
-TOOLCHAIN="$KERNEL_DIR/clang"
-LOG="$KERNEL_DIR/log.txt"
-KERNEL_DTB="$KERNEL_DIR/out/arch/arm64/boot/dtb"
-KERNEL_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image"
-KERNEL_IMG_DTB="$KERNEL_DIR/out/arch/arm64/boot/Image-dtb"
-KERNEL_IMG_GZ_DTB="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
-KERNEL_DTBO="$KERNEL_DIR/out/arch/arm64/boot/dtbo.img"
-TG_CHAT_ID="-1001180467256"
-TG_BOT_TOKEN="$TELEGRAM_TOKEN"
-CLANG_VERSION="$1"
+KERNEL_NAME=Kucing
+KERNEL_DIR=$(pwd)
+AK3=${KERNEL_DIR}/AnyKernel3
+TOOLCHAIN=${KERNEL_DIR}/clang
+LOG=${KERNEL_DIR}/log.txt
+KERNEL_DTB=${KERNEL_DIR}/out/arch/arm64/boot/dtb
+KERNEL_IMG=${KERNEL_DIR}/out/arch/arm64/boot/Image
+KERNEL_IMG_DTB=${KERNEL_DIR}/out/arch/arm64/boot/Image-dtb
+KERNEL_IMG_GZ_DTB=${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb
+KERNEL_DTBO=${KERNEL_DIR}/out/arch/arm64/boot/dtbo.img
+TG_CHAT_ID=-1001180467256
+TG_BOT_TOKEN=${TELEGRAM_TOKEN}
+CLANG_VERSION=${1}
 DATE_NAME=$(date +"%Y%m%d")
 COMMIT=$(git log --pretty=format:"%s" -1)
 COMMIT_SHA=$(git rev-parse --short HEAD)
@@ -23,10 +23,10 @@ KERNEL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BUILD_DATE=$(date)
 CAPTION=$(echo -e \
 "Build started
-Date: <code>$BUILD_DATE</code>
-HEAD: <code>$COMMIT_SHA</code>
-Commit: <code>$COMMIT</code>
-Branch: <code>$KERNEL_BRANCH</code>
+Date: <code>${BUILD_DATE}</code>
+HEAD: <code>${COMMIT_SHA}</code>
+Commit: <code>${COMMIT}</code>
+Branch: <code>${KERNEL_BRANCH}</code>
 ")
 
 # Colors
@@ -36,63 +36,63 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 
-export KBUILD_BUILD_USER="Diaz"
-export KBUILD_BUILD_HOST="DroneCI"
-export PATH="$TOOLCHAIN/bin:$PATH"
+export KBUILD_BUILD_USER=Diaz
+export KBUILD_BUILD_HOST=DroneCI
+export PATH=${TOOLCHAIN}/bin:${PATH}
 
 #
 # Clone Clang Compiler
 clone_tc(){
     echo -e "${YELLOW}===> ${BLUE}Downloading kucing Clang${WHITE}"
-    mkdir -p $TOOLCHAIN
-    wget -q https://github.com/Diaz1401/clang/releases/download/$CLANG_VERSION/clang.tar.zst
-    tar xf clang.tar.zst -C $TOOLCHAIN
+    mkdir -p ${TOOLCHAIN}
+    wget -q https://github.com/Diaz1401/clang/releases/download/${CLANG_VERSION}/clang.tar.zst
+    tar xf clang.tar.zst -C ${TOOLCHAIN}
 }
 
 #
 # Clones anykernel
 clone_ak(){
     echo -e "${YELLOW}===> ${BLUE}Cloning AnyKernel3${WHITE}"
-    git clone -q --depth 1 https://github.com/Diaz1401/AnyKernel3.git -b alioth $AK3
+    git clone -q --depth 1 https://github.com/Diaz1401/AnyKernel3.git -b alioth ${AK3}
 }
 
 #
 # tg_sendinfo - sends text to telegram
 tg_sendinfo(){
-    curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" \
+    curl -s "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage" \
         -F parse_mode=html \
         -F text="${1}" \
-        -F chat_id="${TG_CHAT_ID}" &> /dev/null
+        -F chat_id=${TG_CHAT_ID} &> /dev/null
 }
 
 #
 # tg_pushzip - uploads final zip to telegram
 tg_pushzip(){
-    curl -F document=@"$1"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
-        -F chat_id=$TG_CHAT_ID \
-        -F caption="$2" \
+    curl -F document=@${1} "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendDocument" \
+        -F chat_id=${TG_CHAT_ID} \
+        -F caption="${2}" \
         -F parse_mode=html &> /dev/null
 }
 
 #
 # tg_log - uploads build log to telegram
 tg_log(){
-    curl -F document=@"$LOG"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
-        -F chat_id=$TG_CHAT_ID \
+    curl -F document=@${LOG} "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendDocument" \
+        -F chat_id=${TG_CHAT_ID} \
         -F parse_mode=html &> /dev/null
 }
 
 #
 # build_kernel
 build_kernel(){
-    cd "$KERNEL_DIR"
+    cd ${KERNEL_DIR}
     rm -rf out
     mkdir -p out
     BUILD_START=$(date +"%s")
     make O=out cat_defconfig LLVM=1
     make -j$(nproc --all) O=out \
        LLVM=1 \
-       CROSS_COMPILE=aarch64-linux-gnu- |& tee $LOG
+       CROSS_COMPILE=aarch64-linux-gnu- |& tee ${LOG}
     BUILD_END=$(date +"%s")
     DIFF=$((BUILD_END - BUILD_START))
 }
@@ -100,12 +100,12 @@ build_kernel(){
 #
 # build_end - creates and sends zip
 build_end(){
-    if [[ -a "$KERNEL_IMG_GZ_DTB" ]]; then
-    mv "$KERNEL_IMG_GZ_DTB" "$AK3"
-    elif [[ -a "$KERNEL_IMG_DTB" ]]; then
-    mv "$KERNEL_IMG_DTB" "$AK3"
-    elif [[ -a "$KERNEL_IMG" ]]; then
-    mv "$KERNEL_IMG" "$AK3"
+    if [[ -a ${KERNEL_IMG_GZ_DTB} ]]; then
+    mv ${KERNEL_IMG_GZ_DTB} ${AK3}
+    elif [[ -a {$KERNEL_IMG_DTB} ]]; then
+    mv ${KERNEL_IMG_DTB} ${AK3}
+    elif [[ -a ${KERNEL_IMG} ]]; then
+    mv ${KERNEL_IMG} ${AK3}
     else
     echo -e "${YELLOW}===> ${RED}Build failed, sad${WHITE}"
     echo -e "${YELLOW}===> ${GREEN}Send build log to Telegram${WHITE}"
@@ -113,23 +113,23 @@ build_end(){
     exit 1
     fi
     echo -e "${YELLOW}===> ${GREEN}Build success, generating flashable zip..."
-    find $KERNEL_DIR/out/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > $KERNEL_DIR/out/arch/arm64/boot/dtb
-    ls $KERNEL_DIR/out/arch/arm64/boot/
-    cp "$KERNEL_DTBO" "$AK3"
-    cd $AK3
+    find ${KERNEL_DIR}/out/arch/arm64/boot/dts/vendor/qcom -name '*.dtb' -exec cat {} + > ${KERNEL_DIR}/out/arch/arm64/boot/dtb
+    ls ${KERNEL_DIR}/out/arch/arm64/boot/
+    cp ${KERNEL_DTBO} ${AK3}
+    cd ${AK3}
     DTBO_NAME=${KERNEL_NAME}-DTBO-${DATE_NAME}-${COMMIT_SHA}.img
     DTB_NAME=${KERNEL_NAME}-DTB-${DATE_NAME}-${COMMIT_SHA}
     ZIP_NAME=${KERNEL_NAME}-${DATE_NAME}-${COMMIT_SHA}.zip
-    zip -r9 $ZIP_NAME * -x .git .github LICENSE README.md
-    mv "$KERNEL_DTBO" "$AK3/$DTBO_NAME"
-    mv "$KERNEL_DTB" "$AK3/$DTB_NAME"
+    zip -r9 ${ZIP_NAME} * -x .git .github LICENSE README.md
+    mv ${KERNEL_DTBO} ${AK3}/${DTBO_NAME}
+    mv ${KERNEL_DTB} ${AK3}/${DTB_NAME}
     echo -e "${YELLOW}===> ${BLUE}Send kernel to Telegram"
-    tg_pushzip "$ZIP_NAME" "Time taken: <code>$((DIFF / 60))m $((DIFF % 60))s</code>"
+    tg_pushzip ${ZIP_NAME} "Time taken: <code>$((DIFF / 60))m $((DIFF % 60))s</code>"
     echo -e "${YELLOW}===> ${WHITE}Zip name: ${GREEN}${ZIP_NAME}"
     echo -e "${YELLOW}===> ${BLUE}Send dtbo.img to Telegram"
-    tg_pushzip "$DTBO_NAME"
+    tg_pushzip ${DTBO_NAME}
     echo -e "${YELLOW}===> ${BLUE}Send dtb to Telegram"
-    tg_pushzip "$DTB_NAME"
+    tg_pushzip ${DTB_NAME}
     echo -e "${YELLOW}===> ${RED}Send build log to Telegram"
     tg_log
 }
@@ -138,7 +138,7 @@ build_end(){
 # compile time
 clone_tc
 clone_ak
-tg_sendinfo "$CAPTION
+tg_sendinfo "${CAPTION}
 "
 build_kernel
 build_end
